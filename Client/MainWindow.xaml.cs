e7097d351fe8d199910be2cbf5311828150ec5b7
Konstantin Klenitsky <kklenitsky@gmail.com>
2022-11-23 23:09:59 +0300

    Added Graphical Interface  to Client

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Client
 {
@@ -23,6 +25,25 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+            //modifying any series values will also animate and update the chart
+
+            DataContext = this;
         }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
     }
+    
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Server
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
         }
     }
 }
